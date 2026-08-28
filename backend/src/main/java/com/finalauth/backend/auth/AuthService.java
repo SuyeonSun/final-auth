2db0258d7e2561be8b6c6c7e5a7d352b2a21753d
Suyeon Sun <suninisy@gmail.com>
2026-08-28 15:25:05 +0900

    feat: log 추가

--- backend/src/main/java/com/finalauth/backend/auth/AuthService.java
+++ backend/src/main/java/com/finalauth/backend/auth/AuthService.java
@@ -12,6 +12,8 @@ import com.finalauth.backend.common.ApiException;
 import com.finalauth.backend.user.User;
 import com.finalauth.backend.user.UserDto;
 import com.finalauth.backend.user.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,6 +25,8 @@ import java.util.List;
 
 @Service
 public class AuthService {
+
+	private static final Logger log = LoggerFactory.getLogger(AuthService.class);
 
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
@@ -92,7 +96,11 @@ public class AuthService {
 			throw new ApiException(HttpStatus.UNAUTHORIZED, "AUTH_REFRESH_TOKEN_EXPIRED", "다시 로그인해 주세요.");
 		}
 
-		JwtTokenProvider.GeneratedAccessToken accessToken = jwtTokenProvider.generateAccessToken(refreshToken.getUser());
+		User user = refreshToken.getUser();
+		JwtTokenProvider.GeneratedAccessToken accessToken = jwtTokenProvider.generateAccessToken(user);
+
+		log.info("Access token reissued via refresh token: userId={}, useremail={}, expiresAt={}",
+				user.getId(), user.getUseremail(), accessToken.expiresAt());
 
 		return new AccessTokenData(accessToken.token(), formatInstant(accessToken.expiresAt()));
 	}
